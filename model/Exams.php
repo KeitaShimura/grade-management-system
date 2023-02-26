903a26cfb4586f8c8127a9de265bb64a16cdc1d6
KeitaShimura <keitashimura2023@gmail.com>
2023-02-26 18:43:18 +0900

    テスト

--- model/Exams.php
+++ model/Exams.php
@@ -36,14 +36,31 @@ class Exams
         }
     }
 
+    // ここ
     public function get($name)
     {
-        $statement = $this->PDO->prepare("SELECT students.number AS student_number, students.name AS student_name, exams.kokugo, exams.sugaku, exams.eigo, exams.rika, exams.shakai, exams.goukei FROM exams INNER JOIN students ON exams.student_id = students.id INNER JOIN tests ON exams.test_id = tests.id where students.name = :name");
-        $statement->bindParam(":name", $name);
-        if ($statement->execute()) {
-            return $statement->fetch();
-        } else {
-            return false;
+        $statement = $this->PDO->prepare("SELECT students.number AS student_number, students.name AS student_name, exams.kokugo, exams.sugaku, exams.eigo, exams.rika, exams.shakai, exams.goukei FROM exams INNER JOIN students ON exams.student_id = students.id INNER JOIN tests ON exams.test_id = tests.id WHERE tests.name = '前期中間テスト'");
+        // $statement->bindParam(":name", $name);
+        // if ($statement->execute()) {
+        //     return $statement->fetch();
+        // } else {
+        //     return false;
+        // }
+        $statement->execute();
+
+        $exams = $statement->fetchAll();
+
+        foreach($exams as $exam) {
+            echo '<tr>
+                <td class="col-3" style="text-align: left; vertical-align: middle;">'.$exam['student_number'].'</td>
+                <td class="col-3" style="text-align: left; vertical-align: middle;">'.$exam['student_name'].'</td>
+                <td class="col-3" style="text-align: left; vertical-align: middle;">'.$exam['kokugo'].'</td>
+                <td class="col-3" style="text-align: left; vertical-align: middle;">'.$exam['sugaku'].'</td>
+                <td class="col-3" style="text-align: left; vertical-align: middle;">'.$exam['eigo'].'</td>
+                <td class="col-3" style="text-align: left; vertical-align: middle;">'.$exam['rika'].'</td>
+                <td class="col-3" style="text-align: left; vertical-align: middle;">'.$exam['shakai'].'</td>
+                <td class="col-3" style="text-align: left; vertical-align: middle;">'.$exam['goukei'].'</td>
+            </tr>';
         }
     }
 }
--- view/exams/result.php
+++ view/exams/result.php
@@ -7,6 +7,8 @@ require_once "../../controller/ExamsController.php";
 
 $tests_obj = new TestsController();
 $tests = $tests_obj->get();
+$exams_obj = new ExamsController();
+$exams = $exams_obj->get($_GET['name']);
 ?>
 
 <!DOCTYPE html>
@@ -24,15 +26,14 @@ $tests = $tests_obj->get();
     <h1 class="fs-1" style="margin: 50px 0 0 40px;">テスト一覧画面</h1>
     <ul class="nav justify-content-center">
         <?php foreach ($tests as $test) { ?>
-            <a href="result.php?test_name=<?php echo $test['name']; ?>"><?php echo htmlspecialchars($test['name']); ?></a>
-        <?php } ?>
+            <a href="result.php?test_name=<?php echo $test['name']; ?>"><?php echo $test['name']; ?></a>
+        <?php } ?>e
     </ul>
 
     <table class="table" style="margin:30px auto; text-align: center; border-top: 1px solid lightgray; width:80%;">
         <thead style="height: 50px;">
             <tr>
                 <th class="col-3" style="font-weight: bold;">ID</th>
-                <th class="col-3" style="font-weight: bold;">テスト</th>
                 <th class="col-3" style="font-weight: bold;">名前</th>
                 <th class="col-3" style="font-weight: bold;">国語</th>
                 <th class="col-3" style="font-weight: bold;">数学</th>
@@ -43,16 +44,7 @@ $tests = $tests_obj->get();
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($exam['student_number']); ?></td>
-                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($exam['student_name']); ?></td>
-                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($exam['kokugo']); ?></td>
-                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($exam['sugaku']); ?></td>
-                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($exam['eigo']); ?></td>
-                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($exam['rika']); ?></td>
-                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($exam['shakai']); ?></td>
-                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($exam['goukei']); ?></td>
-            </tr>
+            <?php $exams = $exams_obj->get($test['name']); ?>
         </tbody>
 </body>
 
